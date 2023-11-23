df6d2d978280a6d614556fedff0ad87fb4de4f11
Mahlatsek29 <serathikm@gmail.com>
2023-11-23 09:26:20 +0200

    firebase config file

---
+++ src/Firebase/firebase.congfig.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCcMJua3yDxzzTOweyKZJO0cNhb0ffPYmk",
+  authDomain: "atlegile-marketing-solutions.firebaseapp.com",
+  projectId: "atlegile-marketing-solutions",
+  storageBucket: "atlegile-marketing-solutions.appspot.com",
+  messagingSenderId: "184474823085",
+  appId: "1:184474823085:web:a3bef82b0ea4218b445924",
+  measurementId: "G-M4DZHH66ZS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const FIRESTORE_DB = getFirestore(app);
+// const storage =
+const auth = initializeAuth(app);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const firestore = firebase.firestore();
+
+// export { firebase, firestore };
+export { FIRESTORE_DB, auth, firebase, firestore };
