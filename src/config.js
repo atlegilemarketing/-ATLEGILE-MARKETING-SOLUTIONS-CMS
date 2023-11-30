473edfc8e7f7c9aa62d1014a017fd69274d3e445
Mahlatsek29 <serathikm@gmail.com>
2023-11-30 10:44:47 +0200

    Update

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
+import { getFirestore, doc } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// import firebase from 'firebase/app';
+import 'firebase/storage';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,14 +21,23 @@ const firebaseConfig = {
   storageBucket: "atlegile-marketing-solutions.appspot.com",
   messagingSenderId: "184474823085",
   appId: "1:184474823085:web:a3bef82b0ea4218b445924",
-  measurementId: "G-M4DZHH66ZS"
+  measurementId: "G-M4DZHH66ZS",
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
+// const app = initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+// const storage = firebase.storage();
 
-export { firebase, auth, firestore};
+export const db = getFirestore();
+const storage = firebase.storage();
+
+// export const storage = getStorage();
+export { doc };
+
+export { firebase, auth, firestore,storage };
+
+// export default app;
